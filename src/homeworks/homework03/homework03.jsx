caf143f8618edbb872def1f349938292b2f22f51
MaksZhylyk <maxa.zhilik@gmail.com>
2024-11-18 16:47:05 +0100

    homework03

---
+++ src/homeworks/homework03/homework03.jsx
@@ -0,0 +1,12 @@
+import LoginForm from "../../components/LoginForm/loginForm";
+import './homework03.css';
+
+function Homework03(){
+    return(
+        <div className="homework-container">
+        <LoginForm />
+        </div>
+    );
+}
+
+export default Homework03;
